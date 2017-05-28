947d8ed72c5ac3f9c94ca9276e2efa2c3a555eb0
WilmerAllende <wilmer.sanchez@unprg.edu.pe>
2017-05-28 11:44:26 -0500

    quitar js innecesarios

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,7 @@
 class Account < ApplicationRecord
   has_many :deals
   validates :name, presence: true
+  validates :codigo, presence: true
   searchkick word_start: [:codigo, :name]
 
   def search_data
